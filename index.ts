f12c2d14fc5c81c32d15d380ae4f57ebe17189a4
Radim Štěpaník <radim.stepanik@gmail.com>
2017-11-26 02:24:48 +0100

    Training set for tv

--- index.ts
+++ index.ts
@@ -1,15 +1,14 @@
 import {Layer, Network, Trainer, } from 'synaptic';
+import {testSet as test, trainingSet as train}  from './tv-training-set';
 import * as mnist from 'mnist';
-import * as trainSets from './training-sets';
 
-const set = new trainSets.TrainingSet(100);
 
-const trainingSet = set.getTrainingSet();
-const testSet = set.getTestSet();
+const trainingSet = train;
+const testSet = test;
 
-const inputLayer = new Layer(100);
-const hiddenLayer = new Layer(100);
-const outputLayer = new Layer(100);
+const inputLayer = new Layer(3);
+const hiddenLayer = new Layer(5);
+const outputLayer = new Layer(1);
 
 console.log('project layers');
 inputLayer.project(hiddenLayer);
@@ -41,15 +40,22 @@ console.log(myNetwork.activate(testSet[0].input).map(i => {
 
 console.log("------------------------------------");
 
-console.log(myNetwork.activate(testSet[0].input).map(i => {
-    if (i >0.8) {
-        return 1;
-    } else if (i < 0.1) {
-        return 0;
-    } else {
-        return 2;
-    }
-}).toString());
+// console.log(myNetwork.activate(testSet[0].input).map(i => {
+//     if (i >0.8) {
+//         return 1;
+//     } else if (i < 0.1) {
+//         return 0;
+//     } else {
+//         return 2;
+//     }
+// }).toString());
 console.log(testSet[0].output.toString());
+console.log(testSet[1].output.toString());
+console.log(testSet[3].output.toString());
+console.log(testSet[2].output.toString());
+console.log(testSet[15].output.toString());
 
 process.exit(0);
+
+
+// wallDistance,inFrontOfSofa,sofaDistance,ano/ne
